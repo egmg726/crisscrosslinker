d8525666f820c376860e2d1bafdc5855cd35527c
egmg726 <email.egail@gmail.com>
2020-01-22 16:12:00 +1100

    Updated uniprot.fasta with new Uniprot retrieval

--- R/uniprotfasta.R
+++ R/uniprotfasta.R
@@ -12,30 +12,40 @@
 uniprot.fasta <- function(uniprot.id, download.fasta = FALSE, console.message = TRUE,
                           fasta.directory=getwd()){
 
-  uniprot_fasta <- strsplit(getURL(paste0('https://www.uniprot.org/uniprot/',uniprot.id,'.fasta')),'\n')[[1]]
+  # uniprot_fasta <- strsplit(getURL(paste0('https://www.uniprot.org/uniprot/',uniprot.id,'.fasta')),'\n')[[1]]
+  # 
+  # if(TRUE %in% grepl('</html>',uniprot_fasta)){
+  #   #if TRUE --> there is some kind of error in here because it's just a generic error page
+  #   warning('Error page detected --> NA substituted')
+  #   uniprot_fasta_seq <- NA
+  # } else {
+  #   if(console.message == TRUE){
+  #     cat(paste('Getting sequence from Uniprot:',uniprot_fasta[1],'\n'))
+  #   }
+  #   uniprot_fasta_seq <- paste0(uniprot_fasta[2:length(uniprot_fasta)],collapse = '')
+  # }
+  
+  uniprot_fasta <- tryCatch(readLines(curl(paste0('https://www.uniprot.org/uniprot/',uniprot.id,'.fasta'))),
+                            error = function(err){
+                              cat('Error page detected --> NA substituted\n')
+                              return(NA)
+                            })
 
-  if(TRUE %in% grepl('</html>',uniprot_fasta)){
-    #if TRUE --> there is some kind of error in here because it's just a generic error page
-    warning('Error page detected --> NA substituted')
-    uniprot_fasta_seq <- NA
-  } else {
-    if(console.message == TRUE){
-      cat(paste('Getting sequence from Uniprot:',uniprot_fasta[1],'\n'))
-    }
+  if(!is.na(uniprot_fasta)){
     uniprot_fasta_seq <- paste0(uniprot_fasta[2:length(uniprot_fasta)],collapse = '')
   }
-
+  
   #cat(paste('Getting sequence from Uniprot:',uniprot_fasta[1],'\n'))
-  if(length(uniprot_fasta) == 0){
-    #if TRUE --> blank
-    #go through loops again?
-    #can put within a while loop
-
-    warning(paste('Protein name not found on Uniprot: ',uniprot.id))
-    uniprot_fasta_seq <- NA
-  } else {#end if(length(uniprot_fasta) == 0)
-    uniprot_fasta_seq <- paste0(uniprot_fasta[2:length(uniprot_fasta)],collapse = '')
-  } #end else to #end if(length(uniprot_fasta) == 0)
+  # if(length(uniprot_fasta) == 0){
+  #   #if TRUE --> blank
+  #   #go through loops again?
+  #   #can put within a while loop
+  # 
+  #   warning(paste('Protein name not found on Uniprot: ',uniprot.id))
+  #   uniprot_fasta_seq <- NA
+  # } else {#end if(length(uniprot_fasta) == 0)
+  #   uniprot_fasta_seq <- paste0(uniprot_fasta[2:length(uniprot_fasta)],collapse = '')
+  # } #end else to #end if(length(uniprot_fasta) == 0)
 
   #can have a while loop here so that if  uniprot_fasta_seq is NA
   #can try to go through the menu loops
